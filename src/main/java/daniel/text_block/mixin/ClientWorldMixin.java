7ee7cd69ffbc22d107d585260dbff873ddcf6ab3
Krivoshapkin Eduard <buttersus@mail.ru>
2025-08-03 00:31:10 +0900

    fix: use safe mixin

--- src/main/java/daniel/text_block/mixin/ClientWorldMixin.java
+++ src/main/java/daniel/text_block/mixin/ClientWorldMixin.java
@@ -4,19 +4,30 @@ import daniel.text_block.TextBlock;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.item.Item;
 import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Set;
 
 @Mixin(ClientWorld.class)
 public class ClientWorldMixin {
+    @Final
+    @Shadow
+    @Mutable
+    private static Set<Item> BLOCK_MARKER_ITEMS;
 
-
-    @Redirect(method = "<clinit>", at = @At(value = "INVOKE", target = "java/util/Set.of (Ljava/lang/Object;Ljava/lang/Object;)Ljava/util/Set;"))
-    private static <E> Set<E> textBlock$setTextBlockMarker(E e1, E e2) {
-        return (Set<E>) Set.of(e1, e2, TextBlock.TEXT_BLOCK_ITEM);
+    @Inject(method = "<clinit>", at = @At("TAIL"))
+    private static void replaceBlockMarkerItems(CallbackInfo ci) {
+        // Replace the set with your custom items
+        BLOCK_MARKER_ITEMS = Set.of(
+                Items.BARRIER,
+                Items.LIGHT,
+                TextBlock.TEXT_BLOCK_ITEM
+        );
     }
 }
